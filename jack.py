1a88e64860624291e80e8d7540d56c8eeaf59f12
Ondřej Nevěřil <ondra.neveril@centrum.cz>
2025-06-16 10:31:53 +0200

    Přidání grafiky přes pygame - pygame okno

--- jack.py
+++ jack.py
@@ -1,4 +1,8 @@
 from random import randint
+import pygame
+
+pygame.init()
+
 file = open("save.txt")
 p = int(file.readline())
 pp = int(input(f"Kolik chceš vsadit? Minimální sázka je 10, máš celkem {p}. "))
@@ -16,66 +20,79 @@ def výhra(p, m):
 
 k = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11]
 
+screen = pygame.display.set_mode((1000, 800))
+pygame.display.set_caption("Blackajck")
 
-s = 0
-ss = k.pop(randint(0, len(k) - 1))
-if ss >= 10:
-    ss = 10
-if s <= 10 and ss == 1:
-    ss = 11
-s += ss
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255))
 
-d = 0
-dd = k.pop(randint(0, len(k) - 1))
-if dd >= 10:
-    dd = 10
-if d <= 10 and dd == 1:
-    dd = 11
-d += dd
+    pygame.display.flip()
 
-print(f"Dealer má {d} a jednu další kartu")
-
-dd = k.pop(randint(0, len(k) - 1))
-if dd >= 10:
-    dd = 10
-if d <= 10 and dd == 1:
-    dd = 11
-d += dd
-
-while True:
+    s = 0
     ss = k.pop(randint(0, len(k) - 1))
     if ss >= 10:
         ss = 10
     if s <= 10 and ss == 1:
         ss = 11
     s += ss
-    if s > 21:
-        print(f"Máš {s}, prohrál jsi!")
-        prohra(p, pp)
-        exit()
-    else:
-        if input(f"Máš {s}, chceš další kartu? ANO/NE ") == "NE":
-            break
 
-while d < 17:
-    print(f"Dealer  má  {d}")
+    d = 0
     dd = k.pop(randint(0, len(k) - 1))
     if dd >= 10:
         dd = 10
     if d <= 10 and dd == 1:
         dd = 11
     d += dd
-    if d > 21:
-        print(f"Dealer má {d}, prohrál")
+
+    print(f"Dealer má {d} a jednu další kartu")
+
+    dd = k.pop(randint(0, len(k) - 1))
+    if dd >= 10:
+        dd = 10
+    if d <= 10 and dd == 1:
+        dd = 11
+    d += dd
+
+    while True:
+        ss = k.pop(randint(0, len(k) - 1))
+        if ss >= 10:
+            ss = 10
+        if s <= 10 and ss == 1:
+            ss = 11
+        s += ss
+        if s > 21:
+            print(f"Máš {s}, prohrál jsi!")
+            prohra(p, pp)
+            exit()
+        else:
+            if input(f"Máš {s}, chceš další kartu? ANO/NE ") == "NE":
+                break
+
+    while d < 17:
+        print(f"Dealer  má  {d}")
+        dd = k.pop(randint(0, len(k) - 1))
+        if dd >= 10:
+            dd = 10
+        if d <= 10 and dd == 1:
+            dd = 11
+        d += dd
+        if d > 21:
+            print(f"Dealer má {d}, prohrál")
+            výhra(p, pp)
+            exit()
+    print(f"Dealer má {d}")
+    if d > s:
+        print("Dealer má více a proto vyhrál")
+        prohra(p, pp)
+    elif s > d:
+        print("Máš více, než dealer, vyhrál jsi!")
         výhra(p, pp)
-        exit()
-print(f"Dealer má {d}")
-if d > s:
-    print("Dealer má více a proto vyhrál")
-    prohra(p, pp)
-elif s > d:
-    print("Máš více, než dealer, vyhrál jsi!")
-    výhra(p, pp)
-else:
-    print("Máte oba stejně, remíza")
-    fileout.write(str(p))
+    else:
+        print("Máte oba stejně, remíza")
+        fileout.write(str(p))
+    running = False
+pygame.quit()
